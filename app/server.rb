b7398bac6ff8bac9217fccee95954ab9f96ac307
Chuks Opia <chuks24ng@yahoo.co.uk>
2024-03-16 13:56:19 +0100

    respond to multiple PING commands

--- app/server.rb
+++ app/server.rb
@@ -9,13 +9,21 @@ class YourRedisServer
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     puts("Logs from your program will appear here!")
 
-    # instantiate new TCP server
+    # instantiate new TCP Server
     server = TCPServer.new(@port)
-    # wait for client to connect
+    # wait for client to connect by accepting incoming connection & retuns client socket
     client = server.accept
 
-    # respond to PING command
-    client.puts("+PONG\r\n")
+    # read input
+    while line = client.gets
+      input = line.upcase.chomp
+
+      if input == "PING"
+        # respond to PING command
+        client.puts("+PONG\r\n")
+      end
+    end
+
     # close server
     client.close
   end
