7fa31ed220ee3a786a852841f4a103541aa17494
Chuks Opia <chuks24ng@yahoo.co.uk>
2024-03-16 15:47:37 +0100

    Handle concurrent clients using event loop

--- app/server.rb
+++ app/server.rb
@@ -1,37 +1,59 @@
-require "socket"
+# frozen_string_literal: true
 
+require 'socket'
+
+# Redis server
 class YourRedisServer
+  PING_COMMAND = 'PING'
+
   def initialize(port)
     @port = port
+    # instantiate new TCP Server
+    @server = TCPServer.new(@port)
+    # list of clients
+    @clients = []
   end
 
-  def start
+  def start # rubocop:disable Metrics/MethodLength
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    puts("Logs from your program will appear here!")
+    puts('Logs from your program will appear here!')
 
-    # instantiate new TCP Server
-    server = TCPServer.new(@port)
+    loop do
+      # accept multiple connections using event loop pattern
+      # have file descriptors, use IO.select to select items available to be worked on
+      # this way the code is non-blockiing
+      fds_to_watch = [@server, *@clients]
+      item_ready_to_read, = IO.select(fds_to_watch)
 
-    loop {
-      # accept multiple connections
-      Thread.new(server.accept) { |client| handle_client(client) }
-    }
+      item_ready_to_read.each do |item|
+        # if new client is ready to connect
+        if item == @server
+          @clients << @server.accept
+
+          next
+        end
+
+        # if it's not the server, its a client that wants to send new info
+        handle_client(item)
+      end
+    end
   end
 
   private
 
   def handle_client(client)
-    # read input
-    while line = client.gets
-      input = line.upcase.chomp
+    # read input: []
+    line = client.readpartial(1024).upcase.chomp
+    inputs = line.split(/[\r\n]+/)
 
-      if input == "PING"
-        # respond to PING command
-        client.puts("+PONG\r\n")
-      end
+    inputs.each do |input|
+      # respond to PING command
+      client.puts("+PONG\r\n") if input == PING_COMMAND
     end
-
-    # close server
+  rescue EOFError => e
+    # delete client
+    @clients.delete(client)
+    # close connection
     client.close
   end
 end
