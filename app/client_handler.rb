4fe739d7d2d05709a052a79dbcbab86d8173b4d4
Chuks Opia <chuks24ng@yahoo.co.uk>
2024-03-23 16:10:37 +0100

    Implement Empty RDB Transfer for full resync

---
+++ app/client_handler.rb
@@ -0,0 +1,116 @@
+# frozen_string_literal: true
+
+require_relative 'response'
+require_relative 'commands'
+
+# execute commands based on input from client
+class ClientHandler
+  include Response
+  include Commands
+
+  attr_reader :client, :master, :replication_id, :offset, :store
+
+  def initialize(client, master, replication_id, offset, store)
+    @client = client
+    @master = master
+    @replication_id = replication_id
+    @offset = offset
+    # TODO: parent class & store state
+    @store = store
+  end
+
+  def execute_command(commands) # rubocop:disable Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/AbcSize
+    commands.each_with_index do |input, index|
+      case input.upcase
+      when PING_COMMAND
+        respond_to_ping
+      when ECHO_COMMAND
+        respond_to_echo(commands[index + 1])
+      when SET_COMMAND
+        exp = expiry?(commands[index + 3]) ? commands[index + 4] : nil
+        set_value(commands[index + 1], commands[index + 2], exp)
+      when GET_COMMAND
+        resp = get_value(commands[index + 1])
+        client.write(resp)
+      when INFO_COMMAND
+        respond_to_info(commands[index + 1])
+      when REPLCONF_COMMAND
+        client.write(generate_simple_string('OK'))
+      when PSYNC_COMMAND
+        client.write(generate_simple_string('FULLRESYNC * 0'))
+        # send RDS file
+        res = "$#{decoded_hex_rdb.length}#{CRLF}#{decoded_hex_rdb}"
+        client.write(res)
+      end
+    end
+  end
+
+  private
+
+  def respond_to_ping
+    # respond to PING command
+    client.write(generate_simple_string('PONG'))
+  end
+
+  def respond_to_echo(argument)
+    # respond to ECHO command
+    response = generate_bulk_string(argument)
+    client.write(response)
+  end
+
+  def set_value(key, value, exp)
+    # respond to SET command
+    exp_at = exp.nil? ? nil : (exp.to_i / 1000.0).to_f + Time.now.to_f
+    store[key] = { value: value, exp: exp_at }
+
+    client.write(generate_simple_string('OK'))
+  end
+
+  def get_value(key)
+    # respond to GET command
+    val = store.fetch(key, nil)
+
+    return null_bulk_string if val.nil?
+
+    if val[:exp] && (Time.now.to_f > val[:exp])
+      store.delete(key)
+
+      return null_bulk_string
+    end
+
+    generate_bulk_string(val[:value])
+  end
+
+  def expiry?(input)
+    input&.upcase == 'PX'
+  end
+
+  def respond_to_info(parameter)
+    response = replication_info if parameter == 'replication'
+
+    client.write(generate_bulk_string(response))
+  end
+
+  def replication_info
+    # master?
+    role = @master[:port].nil? ? 'master' : 'slave'
+    resp = <<-REPLICATION
+      role:#{role}
+    REPLICATION
+
+    resp = resp.strip
+    resp += "\nmaster_replid:#{replication_id}" if replication_id
+    resp += "\nmaster_repl_offset:#{offset}" if offset
+
+    resp
+  end
+
+  def empty_hex_rdb
+    '524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374'\
+    '696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2'
+  end
+
+  def decoded_hex_rdb
+    [empty_hex_rdb].pack('H*')
+  end
+end
--- app/server.rb
+++ app/server.rb
@@ -5,9 +5,10 @@ require 'date'
 require_relative 'parser'
 require_relative 'response'
 require_relative 'commands'
+require_relative 'client_handler'
 
 # Redis server
-class YourRedisServer # rubocop:disable Metrics/ClassLength
+class YourRedisServer
   include Response
   include Commands
 
@@ -56,94 +57,18 @@ class YourRedisServer # rubocop:disable Metrics/ClassLength
 
   private
 
-  def handle_client(client) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize, Metrics/CyclomaticComplexity
+  def handle_client(client)
     # read input: []
     line = client.readpartial(1024).chomp
     # parse input
     inputs = Parser.parse(line)
 
-    inputs.each_with_index do |input, index|
-      case input.upcase
-      when PING_COMMAND
-        respond_to_ping(client)
-      when ECHO_COMMAND
-        respond_to_echo(client, inputs[index + 1])
-      when SET_COMMAND
-        exp = expiry?(inputs[index + 3]) ? inputs[index + 4] : nil
-        set_value(client, inputs[index + 1], inputs[index + 2], exp)
-      when GET_COMMAND
-        resp = get_value(inputs[index + 1])
-        client.puts(resp)
-      when INFO_COMMAND
-        respond_to_info(client, inputs[index + 1])
-      when REPLCONF_COMMAND
-        client.puts(generate_simple_string('OK'))
-      when PSYNC_COMMAND
-        client.puts(generate_simple_string('FULLRESYNC * 0'))
-      end
-    end
+    ClientHandler.new(client, @master, @replication_id, @offset, @store).execute_command(inputs)
   rescue EOFError
     # delete client
     @clients.delete(client)
     # close connection
     client.close
-  end
-
-  def respond_to_ping(client)
-    # respond to PING command
-    client.puts(generate_simple_string('PONG'))
-  end
-
-  def respond_to_echo(client, argument)
-    # respond to ECHO command
-    response = generate_bulk_string(argument)
-    client.puts(response)
-  end
-
-  def set_value(client, key, value, exp)
-    # respond to SET command
-    exp_at = exp.nil? ? nil : (exp.to_i / 1000.0).to_f + Time.now.to_f
-    @store[key] = { value: value, exp: exp_at }
-
-    client.puts(generate_simple_string('OK'))
-  end
-
-  def get_value(key)
-    # respond to GET command
-    val = @store.fetch(key, nil)
-
-    return null_bulk_string if val.nil?
-
-    if val[:exp] && (Time.now.to_f > val[:exp])
-      @store.delete(key)
-
-      return null_bulk_string
-    end
-
-    generate_bulk_string(val[:value])
-  end
-
-  def expiry?(input)
-    input&.upcase == 'PX'
-  end
-
-  def respond_to_info(client, parameter)
-    response = replication_info if parameter == 'replication'
-
-    client.puts(generate_bulk_string(response))
-  end
-
-  def replication_info
-    role = @master[:port].nil? ? 'master' : 'slave'
-    resp = <<-REPLICATION
-    role:#{role}
-    REPLICATION
-
-    resp = resp.strip
-    resp += "\nmaster_replid:#{@replication_id}" if @replication_id
-    resp += "\nmaster_repl_offset:#{@offset}" if @offset
-
-    resp
   end
 
   def replication_id
@@ -166,19 +91,19 @@ class YourRedisServer # rubocop:disable Metrics/ClassLength
     resp = generate_resp_array(['ping'])
 
     # send PING response
-    master.puts(resp)
+    master.write(resp)
 
     # send REPLCONF response
     listening_port = "REPLCONF listening-port #{@port}".split(' ')
-    master.puts(generate_resp_array(listening_port))
+    master.write(generate_resp_array(listening_port))
 
     # send REPLCONF response
     capabilities = 'REPLCONF capa psync2'.split(' ')
-    master.puts(generate_resp_array(capabilities))
+    master.write(generate_resp_array(capabilities))
 
     # send PSYNC response
     psync = 'PSYNC ? -1'.split(' ')
-    master.puts(generate_resp_array(psync))
+    master.write(generate_resp_array(psync))
   end
 end
 
